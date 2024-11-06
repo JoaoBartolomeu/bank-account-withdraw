1f20649b6d8670f84417b7683673a62928c165a0
JoaoBartolomeu <joaosnpr@gmail.com>
2024-11-06 01:27:51 -0300

    Criado Classe para indicar erros de regras de negocios com mensagens personalizadas

---
+++ src/main/java/org/treinamentoNelio/exceptions/BusinessException.java
@@ -0,0 +1,9 @@
+package org.treinamentoNelio.exceptions;
+
+public class BusinessException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException(String msg){
+        super(msg);
+    }
+}
